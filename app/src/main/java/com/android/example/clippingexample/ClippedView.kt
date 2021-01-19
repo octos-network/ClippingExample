668eadb91456bb7bce17181a65bfc401c7cb9d17
octos-network <octochicken7aber3@gmail.com>
2021-01-19 12:41:17 +0100

    Implantat 1

--- app/src/main/java/com/android/example/clippingexample/ClippedView.kt
+++ app/src/main/java/com/android/example/clippingexample/ClippedView.kt
@@ -89,8 +89,14 @@ class ClippedView @JvmOverloads constructor(
         )
     }
 
-    private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
+    private fun drawBackAndUnclippedRectangle(canvas: Canvas){
+        canvas.drawColor(Color.GRAY)
+        canvas.save()
+        canvas.translate(columnOne,rowOne)
+        drawClippedRectangle(canvas)
+        canvas.restore()
     }
+    
     private fun drawDifferenceClippingExample(canvas: Canvas) {
     }
     private fun drawCircularClippingExample(canvas: Canvas) {

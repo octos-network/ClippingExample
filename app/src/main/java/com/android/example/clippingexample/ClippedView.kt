3176b76d9bfc8c6e06d2b190fbf9add84b96a6a8
octos-network <octochicken7aber3@gmail.com>
2021-01-19 11:24:43 +0100

    drawClipper angezündet

--- app/src/main/java/com/android/example/clippingexample/ClippedView.kt
+++ app/src/main/java/com/android/example/clippingexample/ClippedView.kt
@@ -2,6 +2,7 @@ package com.android.example.clippingexample
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
@@ -56,6 +57,36 @@ class ClippedView @JvmOverloads constructor(
         drawSkewedTextExample(canvas)
         drawTranslatedTextExample(canvas)
         // drawQuickRejectExample(canvas)
+    }
+
+    private fun drawClippedRectangle(canvas: Canvas) {
+        canvas.clipRect(
+            clipRectLeft,clipRectTop,
+            clipRectRight,clipRectBottom
+        )
+
+        canvas.drawColor(Color.WHITE)
+
+        paint.color = Color.RED
+        canvas.drawLine(
+            clipRectLeft,clipRectTop,
+            clipRectRight,clipRectBottom,paint
+        )
+
+        paint.color = Color.GREEN
+        canvas.drawCircle(
+            circleRadius,clipRectBottom - circleRadius,
+            circleRadius,paint
+        )
+
+        paint.color = Color.BLUE
+        // Align the RIGHT side of the text with the origin.
+        paint.textSize = textSize
+        paint.textAlign = Paint.Align.RIGHT
+        canvas.drawText(
+            context.getString(R.string.clipping),
+            clipRectRight,textOffset,paint
+        )
     }
 
     private fun drawBackAndUnclippedRectangle(canvas: Canvas) {
